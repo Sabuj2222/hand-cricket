0ba93c3d62c4c8c8358f77e1c0f01566f9f6a7db
Sabuj2222 <sabujnilias@gmail.com>
2025-06-29 12:46:39 +0530

    updated

--- script.js
+++ script.js
@@ -47,17 +47,3 @@ function playTurn(userInput) {
     }
   }
 }
-
-function updateScore() {
-  document.getElementById("score").innerHTML = `
-    You: ${userScore}/${userWickets} <br>
-    Bot: ${botScore}/${botWickets} ${!isUserBatting ? '(Chasing)' : ''}
-  `;
-}
-
-function logMessage(msg) {
-  const log = document.getElementById("log");
-  const entry = document.createElement("div");
-  entry.textContent = msg;
-  log.prepend(entry);
-}
